0c321af795604fc826ed1b0637a54bb05a3fae87
Jansen Killinger Cara <141461399+Jansen-SENAC@users.noreply.github.com>
2023-08-11 22:08:23 -0300

    Agora sim a primeira versao do programa

---
+++ Calculo do ano de nascimento.py
@@ -0,0 +1,10 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+nome = input ("Digite o seu nome:")
+idade = int(input("Digite sua idade:"))
+print (nome,"o ano que você nasceu é:",2023-idade)
+saldo = float(input("Digite seu saldo:"))
+print(nome,"O seu saldo é",saldo)
+perc = int(input("digite o percentual:"))
+print ("Seu saldo com %:",((saldo*perc)/100)+saldo)
+print (f"Você {nome} Que nasceu em {2023-idade} terá percentual de {perc}%, que será de R${(saldo*perc/100)+saldo}")
